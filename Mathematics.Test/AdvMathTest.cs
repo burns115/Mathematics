853086e2aeab09aed30ce2ebe97e55d74113d2b2
burns115 <rburns@email.neit.edu>
2024-02-06 13:27:44 -0500

    Adding new stuff

---
+++ Mathematics.Test/AdvMathTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics.Test
+{
+    public class AdvMathTest
+    {
+        [Fact]
+        public void TestArea()
+        {
+            var math = new AdvMath();
+            var result = math.CalculateArea(5, 5);
+            Assert.True(result == 25);
+        }
+
+        [Fact]
+        public void TestAverage()
+        {
+            var math = new AdvMath();
+            List<double> numbers = new List<double> { 2.4, 3.7, 4.1, 5.5 };
+            var result = math.CalculateAverage(numbers);
+            Assert.True( result == 3.925);
+        }
+
+        [Fact]
+        public void TestPythagorean()
+        {
+            var math = new AdvMath();
+            var result = math.CalculatePT(3, 4);
+            Assert.True(result == 5);
+        }
+
+        [Fact]
+        public void TestSquared()
+        {
+            var math = new AdvMath();
+            var result = math.CalculateSquared(11);
+            Assert.True(result == 121);
+        }
+    }
+}
---
+++ Mathematics/AdvMath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class AdvMath
+    {
+        public double CalculateArea(double width, double height)
+        {
+            return width * height;
+        }
+
+        public double CalculateAverage(List<double> values)
+        {
+            if (values == null || values.Count == 0)
+            {
+                return 0;
+            }
+
+            double sum = 0;
+
+            foreach (double value in values)
+            {
+                sum += value;
+            }
+            return sum / values.Count;
+        }
+
+        public double CalculateSquared(double num1)
+        {
+            return num1 * num1;
+        }
+
+        public double CalculatePT(double num1, double num2)
+        {
+            return Math.Sqrt(Math.Pow(num1, 2) + Math.Pow(num2, 2));
+        }
+
+    }
+}

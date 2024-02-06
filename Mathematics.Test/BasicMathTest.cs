7f184bb8f5d33f4117853dada67b7c6cb5cc8a0b
burns115 <rburns@email.neit.edu>
2024-02-06 12:51:10 -0500

    Add project files.

---
+++ Mathematics.Test/BasicMathTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics.Test
+{
+    public class BasicMathTest
+    {
+        [Fact]
+        public void TestAddNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.AddNumbers(5, 5);
+            Assert.True(result == 10);
+        }
+
+        [Fact]
+        public void TestSubNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.SubtractNumbers(10, 3);
+            Assert.True(result == 7);
+        }
+
+        [Fact]
+        public void TestMultiplyNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.MultiplyNumbers(5, 6);
+            Assert.True(result == 30);
+        }
+
+        [Fact]
+        public void TestDivideNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.DivideNumbers(50, 5);
+            Assert.True(result == 10);
+        }
+    }
+}
---
+++ Mathematics/BasicMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class BasicMath
+    {
+        public double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double SubtractNumbers(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double MultiplyNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double DivideNumbers(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
